9d0ea2fac14f1af8db7ae4759dfee45f6628f1d5
leaviki90 <leaviki90@gmail.com>
2021-11-03 21:18:19 +0100

    Console log successfully removed

--- main.js
+++ main.js
@@ -28,24 +28,17 @@ let onPauseValue;
 const timerInit = () => {
   let diff = Date.now() - start;
   let currentSeconds = Math.floor((initialValue - diff) / 1000);
-  // console.log(Math.floor((initialValue - diff) / 1000));
   let minutes = (currentSeconds / 60) >> 0;
   let seconds = currentSeconds - minutes * 60;
   onPauseValue = currentSeconds * 1000;
 
-  // console.log(currentSeconds);
-
   if (diff > initialValue) {
-    console.log("ended");
     displayPause.innerText = "RESTART";
   } else if (pauseIt) {
-    console.log("paused");
     displayPause.innerText = "START";
   } else {
     circlePercent = currentSeconds / (initialSeconds / 100);
-    console.log(circlePercent);
     circleClass.style.strokeDashoffset = `calc(1030 - 10.3 * ${circlePercent})`;
-
     displayTime.innerText =
       (minutes > 9 ? minutes : "0" + minutes) +
       ":" +
@@ -56,21 +49,18 @@ const timerInit = () => {
 };
 
 const pauseTimer = () => {
-  console.log("pause function");
   displayPause.innerText = "RESUME";
   pauseIt = true;
   initialValue = onPauseValue;
 };
 
 const stopTimer = () => {
-  console.log("stop function");
   circleClass.style.strokeDashoffset = `calc(1030 - 10.3 * 100)`;
   displayPause.innerText = "START";
   pauseIt = true;
 };
 
 const startTimer = () => {
-  console.log("start function");
   displayPause.innerText = "PAUSE";
   pauseIt = false;
   start = Date.now();
@@ -78,14 +68,11 @@ const startTimer = () => {
 };
 
 const resumeTimer = () => {
-  console.log("resume function");
   displayPause.innerText = "START";
   pauseIt = false;
   start = Date.now();
   timerInit();
 };
-
-// startTimer();
 
 //EVENT LISTENERS
 iconSettings.addEventListener("click", () => {
@@ -105,7 +92,6 @@ closeBtn.addEventListener("click", () => {
 form.addEventListener("click", (e) => {
   const clicked = e.target;
   const clickedParent = clicked.closest(".form-group");
-
   if (clicked.closest(".icon-arrow-up")) {
     ++clickedParent.querySelector("input").value;
   } else if (clicked.closest(".icon-arrow-down")) {
@@ -133,11 +119,9 @@ displayPause.addEventListener(
 controls.addEventListener("click", (e) => {
   const clicked = e.target;
   const clickedParent = clicked.closest("label");
-
   if (clickedParent) {
     stopTimer();
     circlePercent = 100;
-
     if (clickedParent.getAttribute("for") === "control1") {
       displayTime.innerText = formatMsToMMSS(pomTime);
       initialValue = pomTime;
@@ -158,20 +142,14 @@ formatMsToMMSS = (milis) => {
   return new Date(milis).toISOString().substr(14, 5);
 };
 
-// document.documentElement.style.setProperty('--main-color', 'blue');
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   const fontKumbh = document.querySelector("button.font-kumbh");
   const fontRoboto = document.querySelector("button.font-roboto");
   const fontSpace = document.querySelector("button.font-space");
-
   pomTime = document.getElementById("pomodoro").value * 60000;
-  console.log(pomTime);
   shortTime = document.getElementById("short-break").value * 60000;
-  console.log(shortTime);
   longTime = document.getElementById("long-break").value * 60000;
-  console.log(longTime);
 
   stopTimer();
   displayTime.innerText = formatMsToMMSS(pomTime);
@@ -203,13 +181,11 @@ form.addEventListener("submit", (e) => {
   } else {
     document.documentElement.style.setProperty("--main-color", "#D881F8");
   }
-
   settings.classList.remove("active");
 });
 
 fontsContainer.addEventListener("click", (e) => {
   const clicked = e.target;
-
   const clickedParent = clicked.closest("button");
 
   if (clickedParent) {
@@ -222,7 +198,6 @@ fontsContainer.addEventListener("click", (e) => {
 
 colorsContainer.addEventListener("click", (e) => {
   const clicked = e.target;
-
   const clickedParent = clicked.closest("button");
 
   if (clickedParent) {
